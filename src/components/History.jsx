7df79df82bf6909d4abe48e6a9a6c6fae34f0344
Saqib-17 <sakib17shahidul@gmail.com>
2025-04-21 21:31:56 +0600

    Fixed the page margin for navbar and whole website

---
+++ src/components/History.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from '../assets/logo.png';
+
+function HistoryPage() {
+  return (
+    <div className="lexend-font min-h-screen flex flex-col">
+      
+
+      <main className="flex-grow my-20 lg:px-16 relative w-full">
+        {/* This page is intentionally blank for now */}
+        <div className="text-center text-2xl text-gray-500">
+          This is the History page.
+        </div>
+      </main>
+
+      <footer>
+        <div className="navbar bg-base-100 px-16 bg-secondary-color py-16"></div>
+      </footer>
+    </div>
+  );
+}
+
+export default HistoryPage;
